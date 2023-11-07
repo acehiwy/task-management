259543ea642db87ab046d3a1e2addcbfda4fabdc
wichaya <acehiwy.t@gmail.com>
2023-11-07 14:54:23 +0000

    refac: redirect root route to `/api`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
@@ -16,6 +15,6 @@ import { PrismaModule } from 'src/common/prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
