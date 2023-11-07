0845f9b4b286598c4f07f2a9afb0f7850aeac834
wichaya <acehiwy.t@gmail.com>
2023-11-07 13:08:34 +0000

    refac: add `status` property to `UpdateTaskApiPayload` class

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,9 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { $Enums, Task as PrismaTask } from '@prisma/client';
 import {
   CreateTaskApiPayload,
   CreateTaskPayload,
 } from 'src/tasks/dto/create-task.dto';
 
-export class UpdateTaskApiPayload extends PartialType(CreateTaskApiPayload) {}
+export class UpdateTaskApiPayload extends PartialType(CreateTaskApiPayload) {
+  @ApiProperty({ enum: Object.values($Enums.TaskStatus) })
+  status?: PrismaTask['status'];
+}
 
 export type UpdateTaskPayload = Partial<CreateTaskPayload>;
